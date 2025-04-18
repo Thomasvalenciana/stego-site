70ed7f7c928fafef7e7fbbe8a073dc4e3c7c386b
Thomas Stephan Valenciana <thomasvalenciana@Mac.home>
2025-04-17 19:46:55 -0500

    l

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000', // proxy API requests to Flask
-    },
+  build: {
+    outDir: 'dist',
   },
-})
+});

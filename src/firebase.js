f3a03d83471deeb809384536865099e05c899f0d
Thomas Stephan Valenciana <thomasvalenciana@Mac.home>
2025-04-17 23:01:58 -0500

    jaidjsi

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getAuth} from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore'; // 🆕
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBCQUWqiWqt4ewl4zd8KaNck3jFS49eDqw",
   authDomain: "web1-52d67.firebaseapp.com",
@@ -20,4 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const auth=getAuth(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app); // ✅ add this line
